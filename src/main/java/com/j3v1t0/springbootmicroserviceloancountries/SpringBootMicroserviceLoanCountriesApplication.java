6f84f6a897b8dcbc9243be04481d3738d4423f74
Alfonso Almonte <alfonsoalmonte@gmail.com>
2023-01-26 09:40:21 -0400

    Creating project

---
+++ src/main/java/com/j3v1t0/springbootmicroserviceloancountries/SpringBootMicroserviceLoanCountriesApplication.java
@@ -0,0 +1,13 @@
+package com.j3v1t0.springbootmicroserviceloancountries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceLoanCountriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceLoanCountriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/j3v1t0/springbootmicroserviceloancountries/SpringBootMicroserviceLoanCountriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j3v1t0.springbootmicroserviceloancountries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroserviceLoanCountriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
